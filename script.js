a3d0da3c6362799add4ce4b4cc4c9259a512b19e
aki613 <107912112+aki613@users.noreply.github.com>
2023-12-20 15:55:13 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-// Smooth Scrool Sample
-
-$(function(){
-
-   $('a[href^="#"]').click(function() {
-
-      // 初期設定：移動時間(ms)と頭出し位置
-      var speed = 500;
-      var offset = -0;
-
-      // アンカーを取得
-      var anchor = $(this).attr("href");
-
-      // ターゲットの位置を取得
-      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
-      var position = target.offset().top + offset;
-
-      // スクロール（アニメーション）
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-
-      return false;
-
-   });
-
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 1800,
+    arrows: false,
+  });
 });
 
-$(function() {
-	$('body').fadeIn(1000); //1秒かけてフェードイン！
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	duration: 1000,//時間指定(1000＝1秒)
+	strokeWidth: 0.2,//進捗ゲージの太さ
+	color: '#555',//進捗ゲージのカラー
+	trailWidth: 0.2,//ゲージベースの線の太さ
+	trailColor: '#bbb',//ゲージベースの線のカラー
+	text: {//テキストの形状を直接指定				
+		style: {//天地中央に配置
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-30px 0 0 0',//バーより上に配置
+			transform:'translate(-50%,-50%)',
+			'font-size':'1rem',
+			color: '#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
 });
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
